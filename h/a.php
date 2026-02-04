846cc0d9f02d5f32b3a2e5f74f20b77d37c843cd
Chayanee4046 <66010914046@msu.ac.th>
2026-02-04 14:45:11 +0700

    a

--- h/a.php
+++ h/a.php
@@ -1,1 +1,20 @@
-<h1>ชญาณี รุ่งนภากานต์(ตังเม)</h1>
+<?php
+    session_atart();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชญาณี รุ่งนภากานต์(ตังเม)</title>
+</head>
+
+<body>
+<h1>a.php</h1>
+
+<?php
+    $_SESSION['name'] ="ชญาณี รุ่งนภากานต์";
+    $_SESSION['nickname'] = "ตังเม" ;
+    $_SESSION['p1'] = "โซฟา";
+    $_SESSION['p2'] = "ห่วงยาง";
+</body>
+</html>
---
+++ h/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชญาณี รุ่งนภากานต์(ตังเม)</title>
+</head>
+
+<body>
+<h1>หน้าเข้าสู่ระบบ - ชญาณี</h1>
+
+<form method="post" action="">
+Username <input type="text" name="auser" autofocus required> <br>
+Passord <input type="text" name="apw" required> <br>
+<button type="input type" name="apwd" required> <br>
+</form>
+</body>
+</html>
