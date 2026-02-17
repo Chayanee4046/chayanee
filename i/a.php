9eae424905f37b8afdd23f9ba10f00ee8d93b287
chayanee4046 <66010914046@msu.ac.th>
2026-02-17 18:18:56 +0700

    aa

--- i/a.php
+++ i/a.php
@@ -5,11 +5,20 @@
 <title>ชญาณี รุ่งนภากานต์(ตังเม)</title>
 </head>
 
-
-
-
 <body>
 <h1>งาน i -- ชญาณี รุ่งนภากานต์(ตังเม)</h1>
+
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM 'regions'";
+$rs = mysqli_query($conn, $sql);
+while ($data = misqli_fetch_array($rs)){
+    echo $data['r_id']. "<br>";
+    echo $data['r_name']. "<hr>"
+}
+
+mysqli_close($conn);
+?>
 
 </body>
 </html>
