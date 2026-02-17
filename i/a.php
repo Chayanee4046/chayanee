76e4d41cf7ec7b239e63af2dd0e4fe6587a36eda
chayanee4046 <66010914046@msu.ac.th>
2026-02-17 17:53:37 +0700

    bdgf

---
+++ i/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชญาณี รุ่งนภากานต์(ตังเม)</title>
+</head>
+
+<body>
+<h1>งาน i -- ชญาณี รุ่งนภากานต์(ตังเม)</h1>
+
+</body>
+</html>
