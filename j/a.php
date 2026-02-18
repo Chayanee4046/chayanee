de9a687ace29cfa032a51cee9c3ee8041d750f0d
chayanee4046 <66010914046@msu.ac.th>
2026-02-18 13:49:13 +0700

    aa

---
+++ j/a.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>งาน j 66010914046 ชญาณี รุ่งนภากานต์ (ตังเม) </title>
+</head>
+
+<body>
+
+
+   <h1>งาน j 66010914046 ชญาณี รุ่งนภากานต์ (ตังเม) </h1>
+   <br>
+   <img src="images/1.jpg" width="400">
+
+   
+</body>
+</html>
